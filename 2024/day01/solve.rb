9d653a92b0848bc2066d3fbd27e0f64b5d20d55e
sustainablelobster <sustainablelobster@protonmail.com>
2025-12-01 09:43:31 -0500

    polish 2024-12-01

--- 2024/day01/solve.rb
+++ 2024/day01/solve.rb
@@ -1,26 +1,20 @@
-left_list = []
-right_list = []
-
-File.foreach(ARGV[0]) do |line|
-  numbers = line.scan(/\d+/)
-  left_list.push(numbers[0].to_i)
-  right_list.push(numbers[1].to_i)
+pairs = ARGF.each_line.map do |line|
+  line.scan(/\d+/).map!(&:to_i)
 end
 
-left_list = left_list.sort
-left_hash = left_list.to_h { |n| [n, 0] }
-right_list = right_list.sort
-answer1 = 0
-answer2 = 0
+left_list, right_list = pairs.transpose
+left_list.sort!
+right_list.sort!
 
-left_list.zip(right_list).each do |left, right|
-  answer1 += (left - right).abs
-  left_hash[right] += 1 if left_hash.has_key? right
-end
-
-left_list.each do |n|
-  answer2 += n * left_hash[n]
+answer1 = left_list.zip(right_list).sum do |left, right|
+  (left - right).abs
 end
 
 puts "Answer 1: #{answer1}"
+
+right_tally = right_list.tally
+answer2 = left_list.sum do |n|
+  n * right_tally.fetch(n, 0)
+end
+
 puts "Answer 2: #{answer2}"
